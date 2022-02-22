ac325c5e3dca40aad458e7c370e897c97b9d0fdc
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-02-22 23:22:39 +0530

    iris flower dataset

---
+++ classification/KNN/iris_flower.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from knn_scratch import KNN
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=312
+)
+
+K = 7
+
+knn_clf = KNN(k=K)
+knn_clf.fit(X_train=X_train, y_train=y_train)
+predictions = knn_clf.predict(X_test)
+
+acc = np.sum(predictions == y_test)/len(y_test)
+print(acc)
--- classification/KNN/knn_scratch.py
+++ classification/KNN/knn_scratch.py
@@ -1,8 +1,10 @@
+from collections import Counter
 import numpy as np
 
 
-def euclidean_distance(test_inputs, train_inputs):
-    return np.sqrt(np.sum((train_inputs - test_inputs) ** 2))
+def euclidean_distance(test_input, train_inputs):
+    abs_diff = np.abs(train_inputs - test_input)
+    return np.sqrt(np.sum(np.power(abs_diff, 2), axis=1))
 
 
 class KNN:
@@ -11,18 +13,23 @@ class KNN:
         self.distance_measurement = distance_measurement
 
     def fit(self, X_train, y_train):
-        pass
+        self.X_train = X_train
+        self.y_train = y_train
 
-    def calculate_distance(self, test_X):
+    def calculate_distance(self, test_input):
         distances = []
         if self.distance_measurement == "euclidean":
             distances = euclidean_distance(
-                train_inputs=self.X_train, test_inputs=test_X
+                train_inputs=self.X_train, test_input=test_input
             )
         return distances
 
     def predict(self, test_X):
-        distances = self.calculate_distance(test_X=test_X)
+        y_pred = [self.predict_for_x(x) for x in test_X]
+        return y_pred
+
+    def predict_for_x(self, test_input):
+        distances = self.calculate_distance(test_input=test_input)
         indices = np.argsort(distances)
 
         # TODO: handle distance tie
@@ -30,5 +37,5 @@ class KNN:
         k_nearest_neighbors = self.y_train[k_indices]
 
         # TODO: handle voting tie
-        predicted_outputs = self.y_train[k_indices]
-        return predicted_outputs
+        predicted_output = Counter(k_nearest_neighbors).most_common(1)
+        return predicted_output[0][0]
