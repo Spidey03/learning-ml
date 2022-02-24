55d17cedcd9ef4699f323c732c01fb24e1b420fa
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-02-24 07:47:14 +0530

    tuning

--- classification/KNN/iris_flower.py
+++ classification/KNN/iris_flower.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from knn_tuning import KNN_tuning
 from knn_scratch import KNN
 
 iris = datasets.load_iris()
@@ -9,10 +10,10 @@ X, y = iris.data, iris.target
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=312
 )
+K = [1, 2, 3, 5, 7, 8, 9, 10, 100]
 
-K = 10
-
-knn_clf = KNN(k=K)
+best_k = KNN_tuning(K=K, X_train=X_train, y_train=y_train)
+knn_clf = KNN(k=best_k)
 knn_clf.fit(X_train=X_train, y_train=y_train)
 predictions = knn_clf.predict(X_test)
 
---
+++ classification/KNN/knn_tuning.py
@@ -0,0 +1,23 @@
+from typing import List
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+
+from knn_scratch import KNN
+
+
+def KNN_tuning(K: List[int], X_train, y_train):
+    accuracy = []
+    X_train, X_valid, y_train, y_valid = train_test_split(
+        X_train, y_train, test_size=0.2, random_state=312
+    )
+    for k in K:
+        knn_clf = KNN(k=k)
+        knn_clf.fit(X_train=X_train, y_train=y_train)
+        predictions = knn_clf.predict(X_valid)
+        acc = np.sum(predictions == y_valid)/len(y_valid)
+        accuracy.append(acc)
+
+    best_accuracy_idx = accuracy.index(max(accuracy))
+    print(accuracy[best_accuracy_idx], K[best_accuracy_idx])
+    return K[best_accuracy_idx]
