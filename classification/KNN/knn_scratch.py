cadc51951f74693dd81a60eae42fe572fc84bb10
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-02-25 23:36:54 +0530

    distance measures

--- classification/KNN/knn_scratch.py
+++ classification/KNN/knn_scratch.py
@@ -1,11 +1,43 @@
 from collections import Counter
+
 import numpy as np
 
 
+def manhattan_distance(test_input, train_inputs):
+    """
+    Manhattan distance measure also known as L1 Norm
+    => SUM(|X-Y|)
+    :param test_input:
+    :param train_inputs:
+    :return:
+    """
+    return np.sum(np.abs(train_inputs - test_input))
+
+
 def euclidean_distance(test_input, train_inputs):
+    """
+    Euclidean distance measure also known as L2 Norm
+    => SQRT(SUM((X-Y)^2))
+
+    :param test_input:
+    :param train_inputs:
+    :return: distance from training inputs to test input
+    """
     abs_diff = np.abs(train_inputs - test_input)
     return np.sqrt(np.sum(np.power(abs_diff, 2), axis=1))
 
+
+def minkowski_distance(test_input, train_inputs, p: int):
+    """
+    Minkowski distance is genarlization of both L1 & L2.
+
+    => (SUM((X-Y)^P))^(1/P)
+    :param test_input:
+    :param train_inputs:
+    :return:
+    """
+    abs_diff = np.abs(train_inputs - test_input)
+    return np.power(np.sum(np.power(abs_diff, p), 1/p, axis=1))
 
 class KNN:
     def __init__(self, k: int = 3, distance_measurement: str = "euclidean"):
